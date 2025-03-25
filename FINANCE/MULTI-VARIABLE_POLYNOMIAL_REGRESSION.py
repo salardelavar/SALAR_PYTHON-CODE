37626f66ef7432676e1668d35723220ea04a5582
Salar Delavar Qashqai <44362930+salardelavar@users.noreply.github.com>
2025-03-25 22:46:23 +0330

    Update MULTI-VARIABLE_POLYNOMIAL_REGRESSION.py

--- FINANCE/MULTI-VARIABLE_POLYNOMIAL_REGRESSION.py
+++ FINANCE/MULTI-VARIABLE_POLYNOMIAL_REGRESSION.py
@@ -7,15 +7,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
-
+#-------------------------------------------------------
 # Generate random data
 NUM_SIM = 1000
 X = np.random.rand(NUM_SIM, 4)  # NUM_SIM samples with 4 features
 Y = 3*X[:,0]**2 + 2*X[:,1] + 1.5*X[:,2]**3 + 0.5*X[:,3] + np.random.randn(NUM_SIM) * 0.1  # Nonlinear relationship
-print(X)
+
 # Split data into training and testing sets
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
-
+#-------------------------------------------------------
 # List of different models
 models = {
     "Linear": LinearRegression(),
